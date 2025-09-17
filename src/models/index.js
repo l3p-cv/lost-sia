d3a491a9dd4ec1a7112bae904780a20a35fbdddf
Jonas <mail@jonasgoebel.net>
2025-09-17 09:12:02 +0200

    added models export file

---
+++ src/models/index.js
@@ -0,0 +1,8 @@
+import Annotation from "../Annotation/logic/Annotation"
+import AnnotationMode from "./AnnotationMode"
+import AnnotationStatus from "./AnnotationStatus"
+import AnnotationTool from "./AnnotationTool"
+
+export {
+  Annotation, AnnotationMode,AnnotationStatus, AnnotationTool
+}
